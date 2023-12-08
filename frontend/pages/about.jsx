e43fc3de1abb829a1751d36ec085ed75284b7b38
HemangVora <hemangvora2@gmail.com>
2023-12-09 00:04:03 +0530

    frontend initial commit

---
+++ frontend/pages/about.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+
+      <div className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </div>
+    </div>
+  );
+}
